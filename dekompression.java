02478ecb56ba960e816786b553b5a9887a24fcd4
Yaronye <ronya_budak@hotmail.com>
2021-02-15 15:35:35 +0100

    dekompression DONE
    not very well done but done, could be better optimised (dekompression funktion too lorge)

--- dekompression.java
+++ dekompression.java
@@ -1,22 +1,16 @@
-
-//package dekompression;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class dekompression{
-    static Scanner scan = new Scanner(System.in);     //DEBUG
+    static Scanner scan = new Scanner(System.in);
     static String input = scan.next();
     static String[] charList = input.split("");
     static List<String> inputList = new ArrayList<String>(Arrays.asList(charList));
-    static String addString = "";
-    static boolean paranthesis = false;
-    //static int tempint = 1;
-    //static String tempstring = "";
+    static int paranthesis = 0;
 
-    public static boolean stringIsInt(String x){
+    public static boolean stringIsInt(String x){        //Checks if the input character is an int. source: https://stackoverflow.com/questions/5439529/determine-if-a-string-is-an-integer-in-java/5439547#5439547
         try{
             Integer.parseInt(x);
             return true;
@@ -25,30 +19,25 @@ public class dekompression{
         }
     }
 
-    public static int doubleInt(String stringInt1, String stringInt2){
+    public static int doubleInt(String stringInt1, String stringInt2){  //smashes 2 following ints into one int, example 2 followed by 3 will give 23.
         String tempint = "";
         tempint += stringInt1;
         tempint += stringInt2;
-        return Integer.parseInt(tempint); //add a return for i?
+        return Integer.parseInt(tempint);
     }
 
-    public static void multiplyString(int x, String y){
-        System.out.println("MULTIPLY STRING"); 
-        String multiString = "";
+    public static String multiplyString(int x, String y){     //Given a string and an int, the string will be repeated the int nr of times, and made into one string
+        String tempString = "";
         for(int i = 0; i < x; i++){
-            multiString += y;
+            tempString += y;
         }
-        addString += multiString;
+        return tempString;
     }
 
-    public static void removeFromList(List<String> list){
-        list.remove(0);
-    }
+    public static String dekompress(int innerint){
+        int tempint = 1;
+        String tempstring = "";
 
-    public static void dekompress(int tempint, String tempstring){
-        
-        
-        //for(int i = 0; i < inputList.length; i++){
         while(inputList.size() != 0){
             if(stringIsInt(inputList.get(0))){              //Check if int
                 if(stringIsInt(inputList.get(1))){
@@ -60,43 +49,36 @@ public class dekompression{
                 }
             }
             else{                                           //if not int
-                if(inputList.get(0).equals("(")){          //if start parantheses
-                    paranthesis = true;
+                if(inputList.get(0).equals("(")){               //if start parantheses
+                    paranthesis += 1;
                     inputList.remove(0);
-                    dekompress(tempint, tempstring);      //recursion
+                    tempstring += dekompress(tempint);          //recursion
+                    tempint = 1;
                     continue;
                 }
-                else if(inputList.get(0).equals(")")){      //if end parentheses
+                else if(inputList.get(0).equals(")")){          //if end parentheses
                     inputList.remove(0);
-                    multiplyString(tempint, tempstring);
-                    paranthesis = false;
-                    tempint = 1;
-                    tempstring = "";
+                    tempstring = multiplyString(innerint, tempstring);
+                    paranthesis -= 1;
                     break;
                 }
                 else{                                       //if an alphabetical character
-                    if(paranthesis){
-                        tempstring += inputList.get(0);
+                    if(paranthesis > 0){
+                        tempstring += multiplyString(tempint, inputList.get(0));
+                        tempint = 1;
                     }
                     else{
-                        multiplyString(tempint, inputList.get(0));
+                        tempstring += multiplyString(tempint, inputList.get(0));
                         tempint = 1;
                     }
                 }
             }
-            //System.out.println("inputList");
-            //System.out.println(inputList.get(0));
             inputList.remove(0);
         }
-        System.out.println("HELLO");
-        System.out.println(tempint);
-        System.out.println("HELLO2");
-        System.out.println(tempstring);
-         //add string here
+        return tempstring;
     }
 
     public static void main(String[] args){
-        dekompress(1, "");
-        System.out.println(addString);
+        System.out.println(dekompress(1));
     }    
 }
